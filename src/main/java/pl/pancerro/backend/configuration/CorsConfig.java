4f568012b7c56ba092979327effbbe4a94b8abd2
Adrian Ławecki <56327053+Pancerro@users.noreply.github.com>
2021-01-25 00:18:30 +0100

    Update CorsConfig.java

--- src/main/java/pl/pancerro/backend/configuration/CorsConfig.java
+++ src/main/java/pl/pancerro/backend/configuration/CorsConfig.java
@@ -5,10 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-
-@Configuration	
+	
 public class CorsConfig {
-    @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration corsConfiguration = new CorsConfiguration();
