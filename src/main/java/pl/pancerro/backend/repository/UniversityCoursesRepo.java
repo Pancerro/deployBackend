5cfeb89566188f1d69f90cee512498aeceaead47
Pancerro <pancerro@gmail.com>
2021-05-08 11:13:48 +0200

    refactor sql

--- src/main/java/pl/pancerro/backend/repository/UniversityCoursesRepo.java
+++ src/main/java/pl/pancerro/backend/repository/UniversityCoursesRepo.java
@@ -8,6 +8,6 @@ import javax.persistence.Tuple;
 import java.util.List;
 
 public interface UniversityCoursesRepo extends JpaRepository<UniversityCourses,Long> {
-    @Query(value = "SELECT wydzialy_uniwersytetu.nazwa_wydzialu as nameDepartments, kierunki_studiow.id_wydzialu, COUNT(kierunki_studiow.id_kierunku) as countCourses FROM kierunki_studiow INNER JOIN wydzialy_uniwersytetu ON kierunki_studiow.id_wydzialu = wydzialy_uniwersytetu.id_wydzialu GROUP BY kierunki_studiow.id_wydzialu;",nativeQuery = true)
+    @Query(value = "SELECT wydzialy_uniwersytetu.nazwa_wydzialu as nameDepartments, kierunki_studiow.id_wydzialu, COUNT(kierunki_studiow.id_kierunku) as countCourses FROM kierunki_studiow INNER JOIN wydzialy_uniwersytetu ON kierunki_studiow.id_wydzialu = wydzialy_uniwersytetu.id_wydzialu GROUP BY kierunki_studiow.id_wydzialu, wydzialy_uniwersytetu.nazwa_wydzialu;",nativeQuery = true)
     List<Tuple> getCountCoursesInDepartment();
 }
